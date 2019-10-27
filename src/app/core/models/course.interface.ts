36fb0d7ba3fa3033a7ab393ad08fe048a128d4ff
gegorov <gegorov@yahoo.com>
2019-10-27 18:12:45 +0300

    add comments to interfaces

--- src/app/core/models/course.interface.ts
+++ src/app/core/models/course.interface.ts
@@ -1,3 +1,12 @@
+/**
+ * This interface is describing a Course model
+ * id: course id, number
+ * title: course title, string
+ * creationDate: date of course creation, Date
+ * duration: course duration (min), number
+ * description: course description, string
+ */
+
 export interface ICourse {
     id: number;
     title: string;
--- src/app/core/models/user.interface.ts
+++ src/app/core/models/user.interface.ts
@@ -1,3 +1,10 @@
+/**
+ * This interface is describing a User model
+ * id: user's id, number
+ * firstName: user's first name, string
+ * lastName: user's last name, string
+ */
+
 export interface IUser {
     id: number;
     firstName: string;
