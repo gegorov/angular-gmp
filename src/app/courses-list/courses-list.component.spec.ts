78ef6ed97883ae3e85308503e4109036829e9ea6
gegorov <gegorov@yahoo.com>
2019-11-04 14:25:48 +0300

    refactor course-list.spec, add service stub

--- src/app/courses-list/courses-list.component.spec.ts
+++ src/app/courses-list/courses-list.component.spec.ts
@@ -6,20 +6,34 @@ import { SearchComponent } from "./search/search.component";
 import { FormsModule } from "@angular/forms";
 import { ServiceModule } from "../core/services/index";
 
+import { CourseService} from "../core/services/index";
+import { COURSES} from "../core/helpers/mock-courses";
+import { of } from "rxjs";
+
+
 describe("CoursesListComponent", () => {
     let component: CoursesListComponent;
     let fixture: ComponentFixture<CoursesListComponent>;
 
+    let courseService: CourseService;
+    const CourseServiceStub: Partial<CourseService> = {
+        getCourses: () => of(COURSES)
+    };
+
+
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [CoursesListComponent, CourseComponent, SearchComponent],
-            imports: [FormsModule, ServiceModule]
+            imports: [FormsModule],
+            providers: [{provide: CourseService, useValue: CourseServiceStub}]
         })
             .compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(CoursesListComponent);
+        courseService = TestBed.get(CourseService);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -48,5 +62,11 @@ describe("CoursesListComponent", () => {
         component.onNotify(data);
         fixture.detectChanges();
         expect(console.log).toHaveBeenCalledWith("Delete movie with ID# ", data);
+    });
+
+    it("should call courseService in ngOnInit", () => {
+        const getCoursesSpy = spyOn(courseService, "getCourses");
+        component.ngOnInit();
+        expect(getCoursesSpy).toHaveBeenCalled();
     });
 });
