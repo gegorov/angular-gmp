dc8e7ef8c1483e957aa3038e4cd7687dbfd6988e
gegorov <gegorov@yahoo.com>
2019-11-11 17:38:38 +0300

    add tests for pipes and directives

--- src/app/courses-list/course/course.component.spec.ts
+++ src/app/courses-list/course/course.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CourseComponent } from "./course.component";
+import { DurationPipe } from "../../shared/pipes/index";
 
 describe("CourseComponent", () => {
     let component: CourseComponent;
@@ -8,7 +9,7 @@ describe("CourseComponent", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [CourseComponent]
+            declarations: [CourseComponent, DurationPipe]
         }).compileComponents();
     }));
 
