f0ccc4c32f12241df7b7065d966d632d23398722
gegorov <gegorov@yahoo.com>
2019-12-02 13:31:26 +0300

    finalize add-course page

--- src/app/add-course-page/add-course-page.component.ts
+++ src/app/add-course-page/add-course-page.component.ts
@@ -1,20 +1,85 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { ICourse } from "../core/models/course.interface";
+import { CourseService } from "../core/services/course-service/course.service";
 
 @Component({
     selector: "app-add-course-page",
     templateUrl: "./add-course-page.component.html",
     styleUrls: ["./add-course-page.component.scss"]
 })
-export class AddCoursePageComponent implements OnInit {
+export class AddCoursePageComponent {
 
-    constructor() {
+    private courseService: CourseService;
+    private router: Router;
+
+    /**
+     * Variable to store course title
+     */
+    public courseTitle: string = "";
+
+    /**
+     * Variable to store course description
+     */
+    public courseDescription: string = "";
+
+    /**
+     * Variable to store course duration
+     */
+    public courseDuration: number = 0;
+
+    /**
+     * Variable to store course date
+     */
+    public courseDate: Date = new Date();
+
+    /**
+     * Variable to store course author
+     */
+    public courseAuthors: string = "";
+
+
+    constructor(courseService: CourseService, router: Router) {
+        this.courseService = courseService;
+        this.router = router;
     }
 
+    /**
+     * method that is called onSubmit
+     */
     public onSubmit(event) {
-        console.log("submited", event);
+        event.preventDefault();
+        this.createCourse();
+        this.router.navigate(["/"]);
     }
 
-    ngOnInit() {
+    /**
+     * Method that is called on Cancel button click
+     */
+    public onCancel() {
+        this.clearFields();
+        this.router.navigate(["/"]);
     }
 
+    private createCourse(): void {
+        const course: ICourse = {
+            creationDate: new Date(this.courseDate),
+            description: this.courseDescription,
+            duration: this.courseDuration,
+            id: 0,
+            title: this.courseTitle,
+            topRated: false
+        };
+
+        this.courseService.addCourse(course);
+        this.clearFields();
+    }
+
+    private clearFields(): void {
+        this.courseAuthors = "";
+        this.courseDate = new Date();
+        this.courseDescription = "";
+        this.courseDuration = 0;
+        this.courseTitle = "";
+    }
 }
